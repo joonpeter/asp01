03b3a8c145a80ba11844cdd518ccc51202f002a5
joonpeter <68453765+joonpeter@users.noreply.github.com>
2025-12-28 17:31:02 +0900

    Create TestController.java

---
+++ board-api/src/main/java/me/jsk/app/controller/TestController.java
@@ -0,0 +1,13 @@
+package me.jsk.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api-test")
+    public String test() {
+        return "WAS CONNECT OK";
+    }
+}
